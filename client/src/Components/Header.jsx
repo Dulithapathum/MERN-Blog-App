d8c087120a9c1476c9d00f053c5bba4c340e83e0
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2024-12-24 20:36:34 +0530

    Header Elemet Add

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,7 +1,34 @@
 import React from "react";
-
+import logo from "../assetes/logo.png";
+import { Link } from "react-router-dom";
+import { AiOutlineClose } from "react-icons/ai";
 const Header = () => {
-  return <nav></nav>;
+  return (
+    <nav>
+      <div className="">
+        <Link to="/" className="">
+          <img src={logo} alt="logo" className="" />
+        </Link>
+        <ul className="">
+          <li>
+            <Link to="profile">Dulitha Pathum</Link>
+          </li>
+          <li>
+            <Link to="create">Create Post</Link>
+          </li>
+          <li>
+            <Link to="authors">Authors</Link>
+          </li>
+          <li>
+            <Link to="logout">Logout</Link>
+          </li>
+        </ul>
+        <button className="">
+          <AiOutlineClose />
+        </button>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
