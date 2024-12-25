d53611a1cc7113158a7f6ec99b8f00da4a3cedcd
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2024-12-25 08:19:56 +0530

    Error Page Ui Design

--- client/src/Pages/ErrorPage.jsx
+++ client/src/Pages/ErrorPage.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <>
+      <div className="flex  flex-col items-center mt-60">
+        <h1 className="text-3xl md:text-6xl  font-bold">Page Not Found</h1>
+        <Link to="/">
+          <button className=" bg-black text-white px-4 py-1 m-6 rounded-lg font-bold">
+            Goto Home{" "}
+          </button>
+        </Link>
+      </div>
+    </>
+  );
 };
 
 export default ErrorPage;
