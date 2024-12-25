e5eafb74d7bac50e04634a07d0eeb072b0f4d741
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2024-12-25 19:40:12 +0530

    Log In Page UI Design

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,7 +1,58 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 const Login = () => {
-  return <div>Login</div>;
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const changeInputHandler = (e) => {
+    setUserData((prevState) => {
+      return { ...prevState, [e.target.name]: e.target.value };
+    });
+  };
+  return (
+    <section className="flex justify-center items-center mb-8">
+      <div className="bg-slate-200 p-10 w-full max-w-[500px] rounded-md m-5 flex flex-col justify-center items-center ">
+        <h2 className="text-center font-bold text-3xl">Sign In</h2>
+        <form action="" className="flex flex-col items-center w-full">
+          <p className="w-full  p-1 text-white  bg-red-600 rounded-md m-1 mt-8">
+            This is error message
+          </p>
+
+          <input
+            type="text"
+            placeholder="Email"
+            name="email"
+            value={userData.email}
+            onChange={changeInputHandler}
+            className="w-full  p-2 m-2 rounded-md outline-none"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={userData.password}
+            onChange={changeInputHandler}
+            className="w-full  p-2 m-2 rounded-md outline-none"
+          />
+
+          <button
+            type="submit"
+            className="p-2 m-2 bg-slate-300 w-full max-w-[400px]  rounded-md"
+          >
+            Login
+          </button>
+          <small className="text-lg">
+            Don't have an account ?{" "}
+            <Link to="/register" className="text-blue-500">
+              Register
+            </Link>
+          </small>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Login;
--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -14,11 +14,13 @@ const Register = () => {
     });
   };
   return (
-    <section>
-      <div>
-        <h2>Sign Up</h2>
-        <form action="">
-          <p>This is error message</p>
+    <section className="flex justify-center items-center">
+      <div className="bg-slate-200 p-10 w-full max-w-[500px] rounded-md m-5 flex flex-col justify-center items-center ">
+        <h2 className="text-center font-bold text-3xl">Sign Up</h2>
+        <form action="" className="flex flex-col items-center w-full">
+          <p className="w-full  p-1 text-white  bg-red-600 rounded-md m-1 mt-8">
+            This is error message
+          </p>
           <input
             type="text"
             placeholder="Full Name"
@@ -26,6 +28,7 @@ const Register = () => {
             value={userData.name}
             onChange={changeInputHandler}
             autoFocus
+            className="w-full p-2 m-2 rounded-md outline-none"
           />
           <input
             type="text"
@@ -33,6 +36,7 @@ const Register = () => {
             name="email"
             value={userData.email}
             onChange={changeInputHandler}
+            className="w-full  p-2 m-2 rounded-md outline-none"
           />
           <input
             type="password"
@@ -40,6 +44,7 @@ const Register = () => {
             name="password"
             value={userData.password}
             onChange={changeInputHandler}
+            className="w-full  p-2 m-2 rounded-md outline-none"
           />
           <input
             type="password"
@@ -47,10 +52,19 @@ const Register = () => {
             name="password"
             value={userData.password2}
             onChange={changeInputHandler}
+            className="w-full  p-2 m-2 rounded-md outline-none"
           />
-          <button type="submit">Register</button>
-          <small>
-            Alredy have an account ? <Link to="/login">Sign In</Link>
+          <button
+            type="submit"
+            className="p-2 m-2 bg-slate-300 w-full max-w-[400px]  rounded-md"
+          >
+            Register
+          </button>
+          <small className="text-lg">
+            Alredy have an account ?{" "}
+            <Link to="/login" className="text-blue-500">
+              Sign In
+            </Link>
           </small>
         </form>
       </div>
