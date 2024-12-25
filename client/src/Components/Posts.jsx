eca90bb7889a023f963dd6aa2ab9b0501df98ed6
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2024-12-25 11:34:48 +0530

    Haddle Home Page Post

--- client/src/Components/Posts.jsx
+++ client/src/Components/Posts.jsx
@@ -4,11 +4,21 @@ import { DUMMY_POSTS } from "../assetes/data";
 const Posts = () => {
   const [posts, setPosts] = useState(DUMMY_POSTS);
   return (
-    <div className=" max-w-[1200px] mx-6  lg:mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {posts.map((post) => (
-        <PostsItem key={post.id} post={post} />
-      ))}
-    </div>
+    <>
+      {posts.length > 0 ? (
+        <div className=" max-w-[1200px] mx-6  lg:mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {posts.map((post) => (
+            <PostsItem key={post.id} post={post} />
+          ))}
+        </div>
+      ) : (
+        <div>
+          <h1 className="text-center text-3xl font-bold my-52">
+            No Post Found
+          </h1>
+        </div>
+      )}
+    </>
   );
 };
 
