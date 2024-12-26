eb09035854df10e55a96c7936dbdf59126e4eb38
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2024-12-26 10:45:07 +0530

    Connect DataBase

--- server/index.js
+++ server/index.js
@@ -1,15 +1,26 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+const DB_URL =
+  "mongodb+srv://Dulitha:DulithaP25@dulithadev.srxch.mongodb.net/?retryWrites=true&w=majority&appName=DulithaDev";
 app.use(cors());
 app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log("server is running");
-});
+mongoose
+  .connect(DB_URL)
+  .then(() => {
+    console.log("Database connected successfully");
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Database connection error:", err);
+  });
