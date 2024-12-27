203f4677cf4b74f269585f606462ba52f66b57a5
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2024-12-27 08:52:04 +0530

    Create Post Router

--- server/Routes/postRoutes.js
+++ server/Routes/postRoutes.js
@@ -1,9 +1,21 @@
 import { Router } from "express";
+import {
+  createPost,
+  getAllPosts,
+  getPost,
+  getCatPosts,
+  getUserPosts,
+  editPost,
+  deletePost,
+} from "../controllers/postController.js";
 const router = Router();
 
-router.get("/", (req, res) => {
-  console.log("post routes");
-  res.json({ message: "Post routes working" });
-});
+router.post("/", createPost);
+router.get("/", getAllPosts);
+router.get("/:id", getPost);
+router.get("/categories/category", getCatPosts);
+router.get("/users/:id", getUserPosts);
+router.patch("/:id", editPost);
+router.delete("/:id", deletePost);
 
 export default router;
