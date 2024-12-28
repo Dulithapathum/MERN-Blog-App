4eb269ea0c00aeb12815805db89f6d6a389a122d
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2024-12-28 10:01:15 +0530

    Authors Page API Intergration

--- client/src/Pages/Authors.jsx
+++ client/src/Pages/Authors.jsx
@@ -1,20 +1,27 @@
-import React, { useState } from "react";
-import { authorsData } from "../assetes/data";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 const Authors = () => {
-  const [authors, setAuthors] = useState(authorsData);
+  const [authors, setAuthors] = useState([]);
+  useEffect(() => {
+    const getAuthors = async () => {
+      const responce = await axios.get(`http://localhost:3000/api/users/`);
+      setAuthors(responce.data);
+    };
+    getAuthors();
+  }, []);
   return (
     <section className="grid  grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-[1200px] mx-auto mt-20 mb-44  ">
       {authors.length > 0 ? (
-        authors.map(({ id, avatar, name, posts }) => (
+        authors.map(({ _id, avatar, name, posts }) => (
           <Link
-            key={id}
-            to={`/posts/users/${id}`}
+            key={_id}
+            to={`/posts/users/${_id}`}
             className="flex justify-center items-center gap-4 m-3 p-2 bg-slate-400 rounded-md hover:shadow-lg"
           >
             <div>
               <img
-                src={avatar}
+                src={`http://localhost:3000/upload/${avatar}`}
                 alt={name}
                 className="w-14 h-14 rounded-full bg-cover"
               />
@@ -26,7 +33,7 @@ const Authors = () => {
           </Link>
         ))
       ) : (
-        <h1 className="text-center text-3xl font-bold my-52">
+        <h1 className="text-center text-3xl font-bold my-32">
           No Authors Found
         </h1>
       )}
